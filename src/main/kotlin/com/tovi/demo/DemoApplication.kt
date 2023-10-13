f331dee35d6f333c60c5cea7712deafe8b2daedd
pwrwpw <dlgustjr9912@gmail.com>
2023-10-13 12:26:49 +0900

    security 설정

--- src/main/kotlin/com/tovi/demo/DemoApplication.kt
+++ src/main/kotlin/com/tovi/demo/DemoApplication.kt
@@ -1,9 +1,10 @@
 package com.tovi.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class DemoApplication
 
 fun main(args: Array<String>) {
